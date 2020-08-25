7232bb20bc6a5f9916323054b7db348a1ebc1064
werty12123 <68688518+werty12123@users.noreply.github.com>
2020-08-25 21:20:29 +0900

    DB

--- 03_DBModeling/goodball_table_sql_queries/stadium.sql
+++ 03_DBModeling/goodball_table_sql_queries/stadium.sql
@@ -4,7 +4,7 @@ CREATE TABLE STADIUM(
     stadium_name VARCHAR2(10) NOT NULL,
     stadium_match_member VARCHAR2(10),
     stadium_reservation_start_time NUMBER NOT NULL,
-    stadium_reservation_last_time NUMBER NOT NULL,
+    stadium_reservation_end_time NUMBER NOT NULL,
     CONSTRAINT FK_STADIUM_BRANCH_NUM FOREIGN KEY(branch_num) REFERENCES BRANCH(branch_num) ON DELETE SET NULL
 );
 
